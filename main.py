8209db77c5cbcd590fd26331aca2419736890cff
eroeb <97666589+eroeb@users.noreply.github.com>
2025-12-27 11:50:57 +0100

    init

---
+++ main.py
@@ -0,0 +1,8 @@
+import sys
+import importlib
+sys.path.append("/home/lisa/Repositories/Private/terrain-shader")
+
+"""
+Reload all modules to reflect recent changes without restarting Blender.
+"Ctrl+shift+O > reopen current world" is not sufficient.
+"""
